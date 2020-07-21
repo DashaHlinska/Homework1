1ccdf7477726bf8ff5f46cef44a788accc8a4b0b
DashaHlinska <glinska.dasha@gmail.com>
2020-07-21 11:54:13 +0300

    add task 3

--- src/main/java/lesson7/Main.java
+++ src/main/java/lesson7/Main.java
@@ -9,7 +9,8 @@ public class Main {
         System.out.println(firstCat.name);
 
         Cat secondCat = new Cat("Persik", "Persian");
-        System.out.println("Second cat: " + secondCat);
+        System.out.println(secondCat.name);
+        System.out.println(secondCat.species);
 
         Cat thirdCat = new Cat("Vasiliy", "american bobtail", 3);
         System.out.println("Third cat: " + thirdCat);
@@ -18,7 +19,8 @@ public class Main {
         System.out.println(firstDog.name);
 
         Dog secondDog = new Dog("Ike", "bulldog");
-        System.out.println("Second dog: " + secondDog);
+        System.out.println(secondDog.name);
+        System.out.println(secondDog.species);
 
         Dog thirdDog = new Dog("Alf","canes", 5);
         System.out.println("Third dog: " + thirdDog);
